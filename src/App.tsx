5f6e179bc0dff1bd2f23504263abbbd7f9595680
Kegen Guyll <kegen.guyll@acretrader.com>
2022-11-10 12:31:50 -0600

    feat: removed components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.css';
-import { Button } from './lib';
 
 function App() {
   return (
-    <div className="App">
-      <Button label="tset" />
-    </div>
+    <div className="App" />
   );
 }
 
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,3 @@
-import Button from './components/Button';
-import Badge from './components/Badge';
 import useDraw from './hooks/draw';
 
-export { Button, Badge, useDraw };
+export default useDraw;
